39d7581fac528e77a62d5c1b02765bb623ad666e
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-03-23 00:04:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function threeSum(arr, target) {
 	for(var i=0; i<n; i++){
 		for(var j=i+1; j<n; j++){
 			for(var k=j+1; j<n; j++){
-				if(abs(arr[i]+arr[j]+arr[k]-target)<sum){
+				if(Math.abs(arr[i]+arr[j]+arr[k]-target)<sum){
 					sum = arr[i]+arr[j]+arr[k];
 				}
 			}
